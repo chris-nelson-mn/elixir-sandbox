330027686e71d2437bda755f553eafbf2f055fa1
Chris Nelson <chris.nelson.mn@gmail.com>
2016-04-30 16:42:28 -0500

    Adding Namespaced modules

--- scripts/elixir-in-action-ch2.ex
+++ scripts/elixir-in-action-ch2.ex
@@ -1,5 +1,11 @@
-defmodule Geometry do
-  def rectangle_area(a, b) do
+defmodule Geometry.Rectangle do
+  def area(a, b) do
     a * b
+  end
+end
+
+defmodule Geometry.Circle do
+  def area(r) do
+    3.1415926 * r * r
   end
 end
