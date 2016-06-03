f41cd007f729ec37b0189b039246004a4edf1e5b
Chris Nelson <chris.nelson.mn@gmail.com>
2016-06-03 17:07:59 -0500

    Add method specs

--- scripts/elixir-in-action-ch2.ex
+++ scripts/elixir-in-action-ch2.ex
@@ -2,6 +2,7 @@ defmodule Geometry.Rectangle do
   @moduledoc "Calculations for rectangles"
 
   @doc "Calculate the area of a rectangle."
+  @spec area(number, number) ::number 
   def area(a, b) do
     a * b
   end
@@ -12,7 +13,10 @@ defmodule Geometry.Circle do
   @pi 3.1415926
 
   @doc "Calculate the circumference given the radius of a circle"
+  @spec circumference(number) ::number
   def circumference(r), do: 2 * @pi * r
+
   @doc "Calculate the area given the radius of a circle"
+  @spec area(number) ::number
   def area(r),          do: @pi * r * r
 end
