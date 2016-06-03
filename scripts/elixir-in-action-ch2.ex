e4ef6fed99660846a63b40630606d6dfff13ef7c
Chris Nelson <chris.nelson.mn@gmail.com>
2016-06-03 16:54:51 -0500

    Using @moduledoc and @doc attributes

--- scripts/elixir-in-action-ch2.ex
+++ scripts/elixir-in-action-ch2.ex
@@ -1,12 +1,18 @@
 defmodule Geometry.Rectangle do
+  @moduledoc "Calculations for rectangles"
+
+  @doc "Calculate the area of a rectangle."
   def area(a, b) do
     a * b
   end
 end
 
 defmodule Geometry.Circle do
+  @moduledoc "Calculations for circles"
   @pi 3.1415926
 
+  @doc "Calculate the circumference given the radius of a circle"
   def circumference(r), do: 2 * @pi * r
+  @doc "Calculate the area given the radius of a circle"
   def area(r),          do: @pi * r * r
 end
